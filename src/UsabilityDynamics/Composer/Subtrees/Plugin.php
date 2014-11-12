a6b63603496b7dcfef7c9026eb2ac3aa2a859a95
andypotanin <andy.potanin@usabilitydynamics.com>
2014-11-12 18:52:01 -0500

    error fixed

---
+++ src/UsabilityDynamics/Composer/Subtrees/Plugin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace UsabilityDynamics\Composer\Subtrees {
+
+	use Composer\Composer;
+	use Composer\IO\IOInterface;
+	use Composer\Plugin\PluginInterface;
+
+	class Plugin implements PluginInterface {
+		public function activate( Composer $composer, IOInterface $io ) {
+			//$installer = new TemplateInstaller( $io, $composer );
+			//$composer->getInstallationManager()->addInstaller( $installer );
+		}
+
+	}
+
+}
