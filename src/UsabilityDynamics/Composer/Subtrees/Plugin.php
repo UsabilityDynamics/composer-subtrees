0986d353992e24d7741673ce536a6962cfea9183
andypotanin <andy.potanin@usabilitydynamics.com>
2014-11-12 19:14:10 -0500

    [ci skip]

--- src/UsabilityDynamics/Composer/Subtrees/Plugin.php
+++ src/UsabilityDynamics/Composer/Subtrees/Plugin.php
@@ -1,12 +1,20 @@
 <?php
-
+/**
+ *
+ */
 namespace UsabilityDynamics\Composer\Subtrees {
 
 	use Composer\Composer;
+	use Composer\EventDispatcher\EventSubscriberInterface;
+	use Composer\Config;
 	use Composer\IO\IOInterface;
 	use Composer\Plugin\PluginInterface;
+	use Composer\Plugin\PluginEvents;
+	use Composer\Plugin\PreFileDownloadEvent;
+	use Composer\Plugin\CommandEvent;
 
 	class Plugin implements PluginInterface {
+
 		public function activate( Composer $composer, IOInterface $io ) {
 			//$installer = new TemplateInstaller( $io, $composer );
 			//$composer->getInstallationManager()->addInstaller( $installer );
